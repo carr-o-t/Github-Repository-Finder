9d5b2d3735802cf8ce49f3b6c223737d4505119a
carr-o-t <pandeystakshi@gmail.com>
2022-08-17 17:37:03 +0530

    added next and previous

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,11 +2,18 @@ import axios from 'axios'
 import React, { useRef, useState } from 'react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { LinkIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon, ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/solid'
+import {
+    LinkIcon,
+    ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
+    ArrowRightIcon,
+    ArrowLeftIcon
+} from '@heroicons/react/solid'
 import RepoCard from '../components/RepoCard'
 import SkeletonLoader from '../components/SkeletonLoader'
 import HeaderProfile from '../components/HeaderProfile'
 import CircleLoader from '../components/CircleLoader'
+import Error from '../components/Error'
 
 function Result() {
 
@@ -16,7 +23,6 @@ function Result() {
 
     const { user } = useParams()
     const [range, setRange] = useState([])
-    const [error, setError] = useState(false)
 
     const userData = useQuery(['gitprofile', user], () => {
         setRange([])
@@ -26,15 +32,11 @@ function Result() {
             const totalPageCount = Math.ceil(data.data ? data.data.public_repos / 6 : 0)
             setRange(new Array(totalPageCount).fill(1))
         },
-        onError: (data) => {
-            setError(true)
-        },
         refetchOnWindowFocus: true
     })
     const userdata = userData.data?.data
-    // console.log(userdata);
 
-    const { data: repoData, refetch, isLoading: loadingRepo } = useQuery(['repodata', currentPage], () => {
+    const { data: repoData, refetch, isLoading: loadingRepo, isError: repoError } = useQuery(['repodata', currentPage], () => {
         return axios.get(`https://api.github.com/users/${user}/repos?page=${currentPage}&per_page=6`)
     }, {
         refetchOnWindowFocus: true
@@ -51,11 +53,10 @@ function Result() {
 
     const handleNext = (next) => {
         nextPage.current.push(next)
-        console.log(nextPage)
     }
 
     if (userData.isLoading) return <CircleLoader />
-    if (error) return <h1>ERROR</h1>
+    if (userData.isError) return <Error />
     return (
         <div className="p-8">
             <div className="mx-auto max-w-3xl">
@@ -75,7 +76,7 @@ function Result() {
                             <SkeletonLoader />
                             <SkeletonLoader />
                             <SkeletonLoader />
-                        </> :
+                        </> : repoError ? <Error /> :
                             repoData?.data?.map((repo) => {
                                 return (
                                     <RepoCard key={repo.name} name={repo.name} description={repo.description} langURL={repo.languages_url} id={repo.id} />
@@ -87,27 +88,24 @@ function Result() {
                     <button className={"px-4 py-1 bg-white shadow-lg rounded flex items-center gap-1" + (prevPage.length === 0 ? 'opacity-50 cursor-not-allowed' : '')} onClick={(e) => {
 
                         let nextElement = prevPage.current.pop()
-                        handlePrev(currentPage)
                         handleNext(currentPage)
                         setCurrentPage(nextElement)
-                        // console.log(prevPage);
                     }}
                         disabled={prevPage.length === 0}
                     >
                         <ArrowLeftIcon className="h-4 w-4 text-gray-600" />
-                        <span className="text-gray-600">prev</span>
+                        <span className="text-gray-600 text-sm">prev</span>
                     </button>
                     <button className={"px-4 py-1 bg-white shadow-lg rounded" + (currentPage === 1 ? 'opacity-50 cursor-not-allowed' : '')} onClick={(e) => {
-                        handlePrev(currentPage)
+                        handleNext(currentPage)
                         setCurrentPage((prev) => prev - 1)
                     }} disabled={currentPage === 1} >
                         <ChevronDoubleLeftIcon className="h-4 w-4 text-gray-600" />
                     </button>
                     {
-
                         range.map((item, index) => {
                             return (
-                                <button key={`${item}${index}`} className={"px-4 py-1 bg-white hover:bg-gray-300 shadow-lg rounded " + (currentPage === index + 1 ? 'bg-yellow-400 text-white font-semibold' : '')} onClick={() => {
+                                <button key={`${item}${index}`} className={"px-4 py-1 bg-gray-200 hover:bg-gray-300 shadow-lg rounded " + (currentPage === index + 1 ? 'bg-yellow-500 text-white font-semibold' : '')} onClick={() => {
                                     handlePrev(currentPage)
                                     setCurrentPage(index + 1)
                                 }}>
@@ -119,20 +117,18 @@ function Result() {
                     <button className={"px-4 py-1 bg-white shadow-lg rounded" + (currentPage === range.length ? 'opacity-50 cursor-not-allowed' : '')} onClick={(e) => {
                         handlePrev(currentPage)
                         setCurrentPage((prev) => prev + 1)
-
                     }}
-                        onChange={(e) => {
-
-                        }}
                         disabled={currentPage === range.length}>
                         <ChevronDoubleRightIcon className="h-4 w-4 text-gray-600" />
                     </button>
-                    <button className={"px-4 py-1 bg-white shadow-lg rounded flex items-center gap-1"} onClick={(e) => {
-                        let prevElement = nextPage.current.pop()
-                        handlePrev(prevElement)
+                    <button className={"px-4 py-1 bg-white shadow-lg rounded flex items-center gap-1" + (nextPage.length === 0 ? 'opacity-50 cursor-not-allowed' : '')} onClick={(e) => {
+                        let prevElement = nextPage?.current.pop()
+                        handlePrev(currentPage)
                         setCurrentPage(prevElement)
                         console.log(nextPage);
-                    }} >
+                    }}
+                        disabled={nextPage.length === 0}
+                    >
                         <span className="text-gray-600 text-sm">next</span>
                         <ArrowRightIcon className="h-4 w-4 text-gray-600" />
                     </button>
