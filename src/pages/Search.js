d8c9ae0b9782ed7e8b525a50cd519c25ac785eec
carr-o-t <pandeystakshi@gmail.com>
2022-08-17 15:35:10 +0530

    fixed errors

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -18,10 +18,10 @@ function Search() {
     <div >
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-sm w-[calc(100%_-_2rem)] px-4 py-16 bg-white shadow-lg rounded-xl ">
         <form onSubmit={handleSubmit} className="flex flex-col gap-8 justify-center text-center ">
-          <label htmlFor="" className="text-lg sm:text-2xl text-gray-600">Enter The Github Username</label>
+          <label htmlFor="" className="text-lg sm:text-2xl text-gray-600">Enter a Github Username</label>
           <input
             type="text"
-            placeholder="username"
+            placeholder="type here"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="max-w-[17rem] w-[calc(100%_-_70px)] mx-auto p-[0.7rem] border border-gray-4000 rounded-md text-gray-500 focus:outline-yellow-500" />
