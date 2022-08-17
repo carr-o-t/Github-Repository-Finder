2619f373ed1849dcd8dab60b61cfe335b09717d9
carr-o-t <pandeystakshi@gmail.com>
2022-08-17 15:34:36 +0530

    added skeleton loader

--- src/components/SkeletonLoader.js
+++ src/components/SkeletonLoader.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function SkeletonLoader() {
-    
     return (
         <div className="bg-white shadow-lg rounded-md px-2 py-4 h-32 max-w-sm w-[calc(100vw_-_64px)] mx-auto">
             <div className="animate-pulse">
-                    <div className="h-2 bg-gray-400 mb-4 rounded"></div>
+                <div className="h-2 bg-gray-400 mb-4 rounded"></div>
                 <div className="flex-1 space-y-2 py-1 mb-1">
                     <div className="h-2 bg-gray-400 rounded col-span-2"></div>
                     <div className="h-2 bg-gray-400 rounded"></div>
@@ -17,8 +16,6 @@ function SkeletonLoader() {
             </div>
         </div>
     )
-
-
 }
 
 export default SkeletonLoader
